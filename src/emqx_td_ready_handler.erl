46ff60959e4bc40e4c6e30c40edd0b3640abd344
Sasha Fonseca <sasha.fonseca@talkdesk.com>
2019-01-14 16:52:27 +0000

    Changes

--- src/emqx_td_ready_handler.erl
+++ src/emqx_td_ready_handler.erl
@@ -3,6 +3,7 @@
 -export([init/2]).
 
 init(Req, State) ->
+    io:format("Got health check request"),
     Resp = cowboy_req:reply(200,
              #{<<"content-type">> => <<"text/plain">>},
              <<"Ready.">>,
