995efa8f0a72ff231162f9972bfb3d6560858b72
Sasha Fonseca <sasha.fonseca@talkdesk.com>
2019-01-23 10:49:18 +0000

    Add metrics endpoint

--- src/emqx_td_ready_handler.erl
+++ src/emqx_td_ready_handler.erl
@@ -2,12 +2,14 @@
 
 -export([init/2]).
 
+-define(SIGNAL_HANDLER, signal_handler).
+
 init(Req, State) ->
-    {Code, Body} = case emqx_signal_handler:rcvd(sigterm) of
+    {Code, Body} = case ?SIGNAL_HANDLER:rcvd(sigterm) of
         true -> {502, <<"Terminating.">>};
         false -> {200, <<"Ready.">>}
     end,
-    Resp = cowboy_req:reply(Code
+    Resp = cowboy_req:reply(Code,
                             #{<<"content-type">> => <<"text/plain">>},
                             Body,
                             Req),
